7fa764235f471eb9748897ba6249e8b8fdb71845
NeAdisa <91559880+NeAdisa@users.noreply.github.com>
2024-05-31 12:26:48 +0500

    Update string.h

--- src/libc/string.h
+++ src/libc/string.h
@@ -1,9 +1,3 @@
-/* DESCRIPTION: E93 C Library String Functions Header File
- * AUTHOR: Erdem Ersoy (eersoy93)
- * COPYRIGHT: Copyright (c) 2022-2024 Erdem Ersoy (eersoy93).
- * LICENSE: Licensed with MIT License. See LICENSE file for details.
- */
-
 #ifndef STRING_H
 #define STRING_H
 
