1ed4022de25acabaf5c73eea8968f28fb1791ac1
NeAdisa <91559880+NeAdisa@users.noreply.github.com>
2024-05-31 12:25:45 +0500

    Update pci_libc.h

--- src/libc/pci_libc.h
+++ src/libc/pci_libc.h
@@ -1,9 +1,3 @@
-/* DESCRIPTION: E93 C Library PCI Functions Header File
- * AUTHOR: Erdem Ersoy (eersoy93)
- * COPYRIGHT: Copyright (c) 2024 Erdem Ersoy (eersoy93).
- * LICENSE: Licensed with MIT License. See LICENSE file for details.
- */
-
 #ifndef PCI_LIBC_H
 #define PCI_LIBC_H
 
