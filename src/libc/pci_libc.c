7e011465bd64502174298b84ecfb66cb925c69b6
NeAdisa <91559880+NeAdisa@users.noreply.github.com>
2024-05-31 12:25:30 +0500

    Update pci_libc.c

--- src/libc/pci_libc.c
+++ src/libc/pci_libc.c
@@ -1,9 +1,3 @@
-/* DESCRIPTION: E93 C Library PCI Functions Source File
- * AUTHOR: Erdem Ersoy (eersoy93)
- * COPYRIGHT: Copyright (c) 2024 Erdem Ersoy (eersoy93).
- * LICENSE: Licensed with MIT License. See LICENSE file for details.
- */
-
 #include "pci_libc.h"
 
 char * get_pci_vendor_str(uint8_t bus, uint8_t slot)
