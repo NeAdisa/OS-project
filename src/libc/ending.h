a4198baef06d9860da2cf22529c5e4e5b465a9b3
NeAdisa <91559880+NeAdisa@users.noreply.github.com>
2024-05-31 12:23:07 +0500

    Update ending.h

--- src/libc/ending.h
+++ src/libc/ending.h
@@ -1,9 +1,3 @@
-/* DESCRIPTION: E93 C Library Ending Functions Header File
- * AUTHOR: Erdem Ersoy (eersoy93)
- * COPYRIGHT: Copyright (c) 2024 Erdem Ersoy (eersoy93).
- * LICENSE: Licensed with MIT License. See LICENSE file for details.
- */
-
 #ifndef ENDING_H
 #define ENDING_H
 
