56bf4d361551cd75354df80efeac9c7c1ee75e24
NeAdisa <91559880+NeAdisa@users.noreply.github.com>
2024-05-31 12:20:43 +0500

    Update pci.c

--- src/drivers/pci.c
+++ src/drivers/pci.c
@@ -1,9 +1,3 @@
-/* DESCRIPTION: E93 PCI Driver Source File
- * AUTHOR: Erdem Ersoy (eersoy93)
- * COPYRIGHT: Copyright (c) 2024 Erdem Ersoy (eersoy93).
- * LICENSE: Licensed with MIT License. See LICENSE file for details.
- */
-
 #include "pci.h"
 
 #include "../cpu/drvutils.h"
