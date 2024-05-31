4347b3c3db4b7e84db6f501eeb0b8c9304e44ebf
NeAdisa <91559880+NeAdisa@users.noreply.github.com>
2024-05-31 12:27:13 +0500

    Update video.h

--- src/libc/video.h
+++ src/libc/video.h
@@ -1,9 +1,3 @@
-/* DESCRIPTION: E93 C Library Video Definitions Header File
- * AUTHOR: Erdem Ersoy (eersoy93)
- * COPYRIGHT: Copyright (c) 2022-2024 Erdem Ersoy (eersoy93).
- * LICENSE: Licensed with MIT License. See LICENSE file for details.
- */
-
 #ifndef VIDEO_H
 #define VIDEO_H
 
