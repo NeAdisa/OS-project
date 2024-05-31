ca2c31c9af7ac2932e479d7fa3a657b3aada7ba6
NeAdisa <91559880+NeAdisa@users.noreply.github.com>
2024-05-31 12:18:52 +0500

    Update registers.h

--- src/cpu/registers.h
+++ src/cpu/registers.h
@@ -1,9 +1,3 @@
-/* DESCRIPTION: E93 CPU Registers Header File
- * AUTHOR: Erdem Ersoy (eersoy93)
- * COPYRIGHT: Copyright (c) 2022-2024 Erdem Ersoy (eersoy93).
- * LICENSE: Licensed with MIT License. See LICENSE file for details.
- */
-
 #ifndef REGISTERS_H
 #define REGISTERS_H
 
