9751b37e51907508ecb8e8817ea4c9caa8e3c18f
NeAdisa <91559880+NeAdisa@users.noreply.github.com>
2024-05-31 12:17:37 +0500

    Update interrupts.asm

--- src/cpu/interrupts.asm
+++ src/cpu/interrupts.asm
@@ -1,8 +1,3 @@
-; DESCRIPTION: E93 Interrupts Assembly File
-; AUTHOR: Erdem Ersoy (eersoy93)
-; COPYRIGHT: Copyright (c) 2022-2024 Erdem Ersoy (eersoy93).
-; LICENSE: Licensed with MIT License. See LICENSE file for details.
-
 [extern isr_handler]
 [extern irq_handler]
 
