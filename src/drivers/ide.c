e206405f3e9e10e966456f26a8a255a3a8903109
NeAdisa <91559880+NeAdisa@users.noreply.github.com>
2024-05-31 12:19:38 +0500

    Update ide.c

--- src/drivers/ide.c
+++ src/drivers/ide.c
@@ -1,9 +1,3 @@
-/* DESCRIPTION: E93 IDE Driver Source File
- * AUTHOR: Erdem Ersoy (eersoy93)
- * COPYRIGHT: Copyright (c) 2024 Erdem Ersoy (eersoy93).
- * LICENSE: Licensed with MIT License. See LICENSE file for details.
- */
-
 #include "../cpu/drvutils.h"
 #include "pci.h"
 
