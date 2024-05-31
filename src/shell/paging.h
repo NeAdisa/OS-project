ab3e50c50c39cd20d1066a8a6c8c1ac5b291c25d
NeAdisa <91559880+NeAdisa@users.noreply.github.com>
2024-05-31 12:28:16 +0500

    Update paging.h

--- src/shell/paging.h
+++ src/shell/paging.h
@@ -1,9 +1,3 @@
-/* DESCRIPTION: E93 Shell Paging Header File
- * AUTHOR: Erdem Ersoy (eersoy93)
- * COPYRIGHT: Copyright (c) 2024 Erdem Ersoy (eersoy93).
- * LICENSE: Licensed with MIT License. See LICENSE file for details.
- */
-
 #ifndef PAGING_H
 #define PAGING_H
 
