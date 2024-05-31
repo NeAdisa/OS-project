bbe51a752d00cc1003da539dbbf884416cb6e8cc
NeAdisa <91559880+NeAdisa@users.noreply.github.com>
2024-05-31 12:20:28 +0500

    Update keyboard.h

--- src/drivers/keyboard.h
+++ src/drivers/keyboard.h
@@ -1,9 +1,3 @@
-/* DESCRIPTION: E93 Keyboard Driver Header File
- * AUTHOR: Erdem Ersoy (eersoy93)
- * COPYRIGHT: Copyright (c) 2022-2024 Erdem Ersoy (eersoy93).
- * LICENSE: Licensed with MIT License. See LICENSE file for details.
- */
-
 #ifndef KEYBOARD_H
 #define KEYBOARD_H
 
