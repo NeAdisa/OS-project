104f7e1eceb7542542719f44ea3d9f5e38a27472
NeAdisa <91559880+NeAdisa@users.noreply.github.com>
2024-05-31 12:21:01 +0500

    Update pci.h

--- src/drivers/pci.h
+++ src/drivers/pci.h
@@ -1,9 +1,3 @@
-/* DESCRIPTION: E93 PCI Driver Header File
- * AUTHOR: Erdem Ersoy (eersoy93)
- * COPYRIGHT: Copyright (c) 2024 Erdem Ersoy (eersoy93).
- * LICENSE: Licensed with MIT License. See LICENSE file for details.
- */
-
 #ifndef PCI_H
 #define PCI_H
 
