6d1e97db54ee34067f98925bbe0cda9c047d1372
NeAdisa <91559880+NeAdisa@users.noreply.github.com>
2024-05-31 12:23:47 +0500

    Update init.h

--- src/libc/init.h
+++ src/libc/init.h
@@ -1,9 +1,3 @@
-/* DESCRIPTION: E93 C Library Shell Init Function Header File
- * AUTHOR: Erdem Ersoy (eersoy93)
- * COPYRIGHT: Copyright (c) 2024 Erdem Ersoy (eersoy93).
- * LICENSE: Licensed with MIT License. See LICENSE file for details.
- */
-
 #ifndef INIT_H
 #define INIT_H
 
