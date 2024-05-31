99504c53267d4e53214ef409fc965f6d10986e9b
NeAdisa <91559880+NeAdisa@users.noreply.github.com>
2024-05-31 12:24:34 +0500

    Update memory.c

--- src/libc/memory.c
+++ src/libc/memory.c
@@ -1,9 +1,3 @@
-/* DESCRIPTION: E93 C Library Memory Functions Source File
- * AUTHOR: Erdem Ersoy (eersoy93)
- * COPYRIGHT: Copyright (c) 2022-2024 Erdem Ersoy (eersoy93).
- * LICENSE: Licensed with MIT License. See LICENSE file for details.
- */
-
 #include "memory.h"
 
 void memory_copy(uint8_t * source, uint8_t * destination, int n_bytes)
