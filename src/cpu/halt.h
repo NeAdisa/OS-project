35ae4a27798baf05a1626946b5dc00a0d2177fe2
NeAdisa <91559880+NeAdisa@users.noreply.github.com>
2024-05-31 12:16:54 +0500

    Update halt.h

--- src/cpu/halt.h
+++ src/cpu/halt.h
@@ -1,9 +1,3 @@
-/* DESCRIPTION: E93 CPU Halting Header File
- * AUTHOR: Erdem Ersoy (eersoy93)
- * COPYRIGHT: Copyright (c) 2024 Erdem Ersoy (eersoy93).
- * LICENSE: Licensed with MIT License. See LICENSE file for details.
- */
-
 #ifndef HALT_H
 #define HALT_H
 
