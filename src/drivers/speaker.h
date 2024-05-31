b0d33cfd2fc74db3d24ea7bcb5aa68d8582adf9b
NeAdisa <91559880+NeAdisa@users.noreply.github.com>
2024-05-31 12:22:16 +0500

    Update speaker.h

--- src/drivers/speaker.h
+++ src/drivers/speaker.h
@@ -1,9 +1,3 @@
-/* DESCRIPTION: E93 PC Speaker Driver Header File
- * AUTHOR: Erdem Ersoy (eersoy93)
- * COPYRIGHT: Copyright (c) 2024 Erdem Ersoy (eersoy93).
- * LICENSE: Licensed with MIT License. See LICENSE file for details.
- */
-
 #ifndef SPEAKER_H
 #define SPEAKER_H
 
