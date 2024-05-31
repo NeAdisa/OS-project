6a8b9c78dd21b5893f6f3aac3e4074c7d27a65a0
NeAdisa <91559880+NeAdisa@users.noreply.github.com>
2024-05-31 12:18:07 +0500

    Update isr.h

--- src/cpu/isr.h
+++ src/cpu/isr.h
@@ -1,9 +1,3 @@
-/* DESCRIPTION: E93 Interrupt Service Routine Header File
- * AUTHOR: Erdem Ersoy (eersoy93)
- * COPYRIGHT: Copyright (c) 2022-2024 Erdem Ersoy (eersoy93).
- * LICENSE: Licensed with MIT License. See LICENSE file for details.
- */
-
 #ifndef ISR_H
 #define ISR_H
 
